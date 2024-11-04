fe39e99ad816155faf308fc7e170e8d261c9ca71
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-11-04 19:07:40 -0400

    new config: added python file(information)

---
+++ info.py
@@ -0,0 +1,6 @@
+VERSION = 0.1
+DESCRIPTION = "The perfect editor for playing music and videos!"
+AUTHOR = "NopAngel"
+LICENSE = "MIT"
+RELEASE_DATE = "2024-11-4"
+GITHUB = "https://github.com/NopAngel"
