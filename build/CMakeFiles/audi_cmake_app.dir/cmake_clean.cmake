7a50dabe71400204dd1573f583becd7c3303b43a
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-11-04 18:49:03 -0400

    edit config: gitignored add!

---
+++ build/CMakeFiles/audi_cmake_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/audi_cmake_app.dir/audi_cmake_app.cpp.obj"
+  "CMakeFiles/audi_cmake_app.dir/audi_cmake_app.cpp.obj.d"
+  "audi_cmake_app.exe"
+  "audi_cmake_app.exe.manifest"
+  "audi_cmake_app.pdb"
+  "libaudi_cmake_app.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/audi_cmake_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
